170ff2d0636b65c9c9d28e3fba198da6ab37ec9e
Chanwoo Jeong <144513143+Chanwoo-Jeong-2000@users.noreply.github.com>
2025-06-14 13:46:27 +0900

    Add files via upload

---
+++ LightGCN/run_DNA-LightGCN.sh
@@ -0,0 +1,12 @@
+
+# Yelp
+python main_DNA-LightGCN.py --dataset Yelp
+python main_LightGCN.py --dataset Yelp
+
+# Gowalla
+python main_DNA-LightGCN.py --dataset Gowalla
+python main_LightGCN.py --dataset Gowalla
+
+# Amazon
+python main_DNA-LightGCN.py --dataset Amazon-CD
+python main_LightGCN.py --dataset Amazon-CD
